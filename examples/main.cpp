6db395ca2b95657632a532630cceeea72f83dc3f
SimonRussia <git.streisand@gmail.com>
2017-12-18 17:48:43 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -12,7 +12,7 @@ int main()
     std::cout<<std::endl<<"hardware_concurrency: "<<std::endl;
     std::cout<<std::thread::hardware_concurrency()<<std::endl;
     std::cout<<std::endl;
-    Client client ("7d10cec10d0b87df392c5aa0d1b13456591c6d0b1fa3b0a9ef2ed232360efc52b131b620b96d37a3c2c96&"); //старый "просроченный" токен
+    Client client ("87b0f4e887b0f4e887b0f4e81f87ef2e05887b087b0f4e8dd928ec42b8c3a9268d740d1"); //старый "просроченный" токен
     if (client.check_connection())
     {
         std::cout<<std::endl<<"Successfully connected!"<<std::endl;
